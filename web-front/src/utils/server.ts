dfb743d1bd1d8e91eb8b8f573353163952c240d8
PBTL <huajian@linkgo.ltd>
2022-10-14 22:51:06 +0800

    updata: axios

--- web-front/src/utils/server.ts
+++ web-front/src/utils/server.ts
@@ -1,3 +1,25 @@
 import axios from "axios";
 
 const server = axios.create({ baseURL: "http://localhost:5000" });
+
+server.interceptors.request.use(
+  (config) => {
+    return config;
+  },
+  (error) => {
+    console.log(error);
+    return Promise.reject(error);
+  }
+);
+
+server.interceptors.response.use(
+  (res) => {
+    if (res.status !== 200) return Promise.reject(new Error(`response code: ${res.status}`));
+    return res;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export { server };
